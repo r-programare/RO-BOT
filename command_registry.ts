9f01c222444e16cd72986e276627e4512e37c3a5
Ică Alexandru-Gabriel <alexandruica703@gmail.com>
2021-10-01 20:33:31 +0300

    (#11) Move each command implementation in its own file
    Now all commands will be cleanly separated in terms of
    implementations, it should make life easier when wanting to implement
    a new command

--- command_registry.ts
+++ command_registry.ts
@@ -1,4 +1,7 @@
 import { BaseCommandInteraction } from 'discord.js';
+import { whoami } from './commands/whoami.js';
+import { server } from './commands/server.js';
+import { ping } from './commands/ping.js';
 
 type CommandDescription = [
     description: string,
@@ -9,25 +12,7 @@ export const cmdDesc = 0;
 export const cmdExec = 1;
 
 export const commands: Record<string, CommandDescription> = {
-    'ping': [
-        'Check if the bot is alive!',
-        async (interaction: BaseCommandInteraction): Promise<void> => {
-            await interaction.reply('Pong!');
-        },
-    ],
-    'server': [
-        'Get server info!',
-        async (interaction: BaseCommandInteraction): Promise<void> => {
-            const result = 'Server name: ' + interaction.guild.name +
-                           '\nMember count: ' + interaction.guild.memberCount;
-            await interaction.reply(result);
-        },
-    ],
-    'whoami': [
-        'Get user info!',
-        async (interaction: BaseCommandInteraction): Promise<void> => {
-            const result = 'User tag: ' + interaction.user.tag + '\nId: ' + interaction.user.id;
-            await interaction.reply(result);
-        },
-    ],
+    'ping': ['Check if the bot is alive!', ping],
+    'server': ['Get server info!', server],
+    'whoami': ['Get user info!', whoami],
 };
